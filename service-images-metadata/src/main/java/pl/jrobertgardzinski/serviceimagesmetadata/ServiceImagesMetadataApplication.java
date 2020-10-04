6e61b678211aa310c4b54d57d2be6a00600d0876
Robert <jrobertgardzinski@gmail.com>
2020-10-04 21:53:56 +0200

    Add initially configured service-images-metadata

---
+++ service-images-metadata/src/main/java/pl/jrobertgardzinski/serviceimagesmetadata/ServiceImagesMetadataApplication.java
@@ -0,0 +1,13 @@
+package pl.jrobertgardzinski.serviceimagesmetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceImagesMetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceImagesMetadataApplication.class, args);
+	}
+
+}
---
+++ service-images-metadata/src/test/java/pl/jrobertgardzinski/serviceimagesmetadata/ServiceImagesMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jrobertgardzinski.serviceimagesmetadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceImagesMetadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
