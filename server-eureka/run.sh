ef29a3d843694968c4608794679c188946b04610
Robert <jrobertgardzinski@gmail.com>
2020-10-04 21:18:03 +0200

    Add Netflix Eureka Server

---
+++ server-eureka/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting Eureka Server"
+echo "********************************************************"
+java -jar /usr/local/server-eureka/server-eureka-0.0.1-SNAPSHOT.jar
