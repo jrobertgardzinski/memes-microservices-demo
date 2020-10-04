30e711446fff9dbc11a20fe4a06a0c8611046d93
Robert <jrobertgardzinski@gmail.com>
2020-10-04 10:29:14 +0200

    Initial commit

---
+++ service-reactive-images/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting Reactive Images Service"
+echo "********************************************************"
+java -jar /usr/local/service-reactive-images/service-reactive-images-0.0.1-SNAPSHOT.jar
---
+++ service-reactive-images/src/test/java/pl/jrobertgardzinski/reactive_images/ReactiveImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jrobertgardzinski.reactive_images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
