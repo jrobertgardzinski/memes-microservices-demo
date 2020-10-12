653516513410d48bcd308009323553725be46a9d
Robert <jrobertgardzinski@gmail.com>
2020-10-12 07:45:54 +0200

    Tweak data for service-tags

--- service-tags/src/main/resources/data.sql
+++ service-tags/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO public.tags (tag, image_ids_array) VALUES ('Miscellanous', '{1,15,16}');
+INSERT INTO public.tags (tag, image_ids_array) VALUES ('Miscellanous', '{1,15,16, 17}');
 INSERT INTO public.tags (tag, image_ids_array) VALUES ('IT', '{1,2,3,4,5,6,7,8,9,10,11,12,13,14,17}');
